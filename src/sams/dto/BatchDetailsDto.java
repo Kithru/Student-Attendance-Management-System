89d9bc3677c0d15556ab879f2bf90b3305bac3cb
Kithru Viduranga <kithruV@icbtcampus.local>
2025-07-19 22:53:49 +0530

    Update dto

--- src/sams/dto/BatchDetailsDto.java
+++ src/sams/dto/BatchDetailsDto.java
@@ -10,4 +10,50 @@ package sams.dto;
  */
 public class BatchDetailsDto {
     
+    private Integer batchNumber;
+    private String batchProName;
+    private String batchStartDate;
+    private String description;
+    private String duration;
+
+    public Integer getBatchNumber() {
+        return batchNumber;
+    }
+
+    public void setBatchNumber(Integer batchNumber) {
+        this.batchNumber = batchNumber;
+    }
+
+    public String getBatchProName() {
+        return batchProName;
+    }
+
+    public void setBatchProName(String batchProName) {
+        this.batchProName = batchProName;
+    }
+
+    public String getBatchStartDate() {
+        return batchStartDate;
+    }
+
+    public void setBatchStartDate(String batchStartDate) {
+        this.batchStartDate = batchStartDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+    
 }
--- src/sams/dto/ProgramDetailsDto.java
+++ src/sams/dto/ProgramDetailsDto.java
@@ -9,5 +9,41 @@ package sams.dto;
  * @author kithruV
  */
 public class ProgramDetailsDto {
-    
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public String getProgramCode() {
+        return programCode;
+    }
+
+    public void setProgramCode(String programCode) {
+        this.programCode = programCode;
+    }
+
+    public String getProgramName() {
+        return programName;
+    }
+
+    public void setProgramName(String programName) {
+        this.programName = programName;
+    }
+
+    private String description;
+    private String duration;
+    private String programCode;
+    private String programName;
 }
