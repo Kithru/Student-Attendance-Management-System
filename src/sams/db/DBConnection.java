ce05a9f244847a6cc359ccf52ba851b75baf05ff
Kithru Viduranga <kithruV@icbtcampus.local>
2025-07-29 00:09:00 +0530

    Add new updates

--- src/sams/db/DBConnection.java
+++ src/sams/db/DBConnection.java
@@ -17,6 +17,7 @@ public class DBConnection {
     private Connection connection;
     
     private DBConnection() throws ClassNotFoundException, SQLException{
+        
         Class.forName("com.mysql.cj.jdbc.Driver");
         connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/SAMS", "root", "12345");
     }
